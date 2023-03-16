b2a98db7ac1c45b5f1ee5d33bcc481f26f0c86ca
Diego Alejandro Lozano Millan <diegoalejandrolozano@hotmail.es>
2023-03-16 13:24:48 -0500

    V4. Se implementa el algoritmo de agrupamiento aglomerativo

--- utils/utils.py
+++ utils/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from matplotlib import pyplot as plt
 
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering
 from sklearn.metrics import silhouette_score
 
 #Construyendo una función que ejecute el método del elbow
@@ -38,29 +38,46 @@ def metodo_elbow(n_grupos:int, dataset:pd.DataFrame) -> None:
     plt.show()
 
 #Construyendo una función que ejecute el método de las siluetas
-def metodo_silueta(n_grupos:int, dataset:pd.DataFrame) -> None:
+def metodo_silueta(n_grupos:int, dataset:pd.DataFrame, agrupamiento:str='kmeans') -> None:
     """Función encargada de graficar el puntaje del coeficiente de siluetas vs
     el número de cluster (k)
     
     Parámetros de entrada:
     
     n_grupos:int -> Número de grupos que quiero visualizar
-    dataset:pd.DataFrame -> Dataset al cual se le aplicará el análisis"""
-    coef_siluetas_score = []
-    for i in range(2, n_grupos+1):
-        km = KMeans(
-            n_clusters=i,
-            init='k-means++',
-            n_init=10,
-            max_iter=300,
-            random_state=0
-        )
-        y_km = km.fit_predict(X=dataset)
-        coef_siluetas_score.append(silhouette_score(dataset, y_km, metric='euclidean'))
+    dataset:pd.DataFrame -> Dataset al cual se le aplicará el análisis
+    agrupamiento:str -> especifica el tipo de agrupamiento al cual se le aplicará el
+    método de siluetas. Las opciones son las siguientes: 'kmeans' o 'aglomerativo'"""
     
+    if agrupamiento not in ['kmeans','aglomerativo']:
+        raise Exception('Error en la selección del tipo de agrupamiento')
+    
+    coef_siluetas_score = []
+
+    if agrupamiento == 'kmeans':
+        for i in range(2, n_grupos+1):
+            km = KMeans(
+                n_clusters=i,
+                init='k-means++',
+                n_init=10,
+                max_iter=300,
+                random_state=0
+            )
+            y_km = km.fit_predict(X=dataset)
+            coef_siluetas_score.append(silhouette_score(dataset, y_km, metric='euclidean'))
+    elif agrupamiento == 'aglomerativo':
+        for i in range(2, n_grupos+1):
+            ac = AgglomerativeClustering(
+                n_clusters=i,
+                metric='euclidean',
+                linkage='complete'
+            )
+            y_ac = ac.fit_predict(X=dataset)
+            coef_siluetas_score.append(silhouette_score(X=dataset, labels=y_ac))
+                
     plt.plot(range(2, n_grupos+1), coef_siluetas_score, marker='o')
     plt.title("Coeficiente de silueta Vs Número de cluster's")
     plt.xlabel('Número de clusters (k)')
-    plt.ylabel('Puntaje coeficiente de silueta')
+    plt.ylabel('Puntaje coeficiente de silueta\nPuntaje máximo: {:.2f}'.format(max(coef_siluetas_score)))
     plt.grid()
     plt.show()
